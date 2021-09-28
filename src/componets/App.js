861db6fa411bcda6458889c8f6b96d15eb2a34ff
Iliya Lunev <lunevig90@gmail.com>
2021-09-28 13:21:45 +0300

    Добавлены стили

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedBackOptions/FeedBackOption';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
 
 export default class App extends Component {
   state = {
@@ -35,18 +37,25 @@ export default class App extends Component {
     const objKey = Object.keys(this.state);
     return (
       <div>
-        <FeedbackOptions
-          options={objKey}
-          onLeaveFeedback={this.onLeaveFeedback}
-        />
-
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={total}
-          positivePercentage={positivePercentage}
-        />
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={objKey}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        {total === 0 ? (
+          <Notification message="No feedback given" />
+        ) : (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          </Section>
+        )}
       </div>
     );
   }
--- src/componets/FeedBackOptions/FeedBackOption.js
+++ src/componets/FeedBackOptions/FeedBackOption.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import s from './FeedBackOption.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <>
       {options.map(option => (
         <button
+          className={s.btn}
           key={option}
           type="button"
           name={option}
